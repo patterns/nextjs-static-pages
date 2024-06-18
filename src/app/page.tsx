c92411c07d0b8d7de192c627d803c59218a834e2
興怡 <>
2024-06-18 02:53:08 -0700

    typscript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from 'next/image'
+import { Suspense } from 'react'
 import { headers } from 'next/headers'
 export const runtime = 'edge'
 
 export default Home
 
-function Home() {
+async function Relayauth() {
   const headersList = headers()
-  let placeholder = 'Missing CF Access JWT header'
-
   if (headersList.has('cf-access-jwt-assertion')) {
     const authorization = headersList.get('cf-access-jwt-assertion')
 
@@ -16,9 +15,14 @@ function Home() {
       headers: { authorization, "Content-Type":"application/json" },
       body: '{"username":"preview@constaninople.edu"}',
     })
-    const user = res.json()
-    if (user.username) placeholder = user.username
+    const user = await res.json()
+    if (user.username) {
+      return <code className="font-mono font-bold">{user.username}</code>
+    }
   }
+  return <code className="font-mono font-bold">Missing CF Access JWT header</code>
+}
+function Home() {
 
 
   return (
@@ -26,7 +30,9 @@ function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by inspecting&nbsp;
-          <code className="font-mono font-bold">{placeholder}</code>
+          <Suspense fallback={<p>Connecting.....</p>}>
+          <Relayauth />
+          </Suspense>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
