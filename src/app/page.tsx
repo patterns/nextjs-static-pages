dceb534e9895ebc5850d0de8b02ce114e3ef39d6
興怡 <>
2024-06-18 02:03:48 -0700

    headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
-import Image from "next/image";
+import Image from 'next/image'
+import { headers } from 'next/headers'
 
-export default function Home() {
+export default Home
+
+
+function Home() {
+  const headersList = headers
+
+  let placeholder = 'Missing CF Access JWT header'
+  if (headersList.has('cf-access-jwt-assertion')) {
+  /*
+    const token = headersList.get('cf-access-jwt-assertion');
+    const body = await fetch();
+    const user = res.json(body);
+
+    return (
+      <code className="font-mono font-bold>${user.username}</code>
+    );
+  */
+
+    placeholder = 'Show Header Here'
+  }
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+          Get started by inspecting&nbsp;
+          <code className="font-mono font-bold">${placeholder}</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
