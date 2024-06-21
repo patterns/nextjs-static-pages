bc5a72ab9fe898ceac61a0687db9e85461ffe7f7
興怡 <>
2024-06-21 02:56:13 -0700

    fetch list

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -15,4 +15,16 @@ export async function fetchIdentify(authorization: string) {
 	}
 }
 
+export async function fetchUsers() {
+	try {
+		const res = await fetch('https://hello-hono-opm.pages.dev/api/users/')
+
+		const data = await res.json()
+		return data
+	} catch (error) {
+		console.log('API users:', error)
+		throw new Error('Fetch users fail.')
+	}
+}
+
 
--- src/app/ui/users-list.tsx
+++ src/app/ui/users-list.tsx
@@ -1,16 +1,38 @@
 ////import { headers } from 'next/headers'
-////import { fetchUsers } from '@/app/lib/data'
+import { fetchUsers } from '@/app/lib/data'
 
 export default async function Users() {
 
-/*
-	const user = await fetchUsers()
-	const debug = JSON.stringify(user)
-	if (debug) {
-		return <code className="font-mono font-bold">{debug}</code>
-	}*/
+  const list = await fetchUsers()
+	//const debug = JSON.stringify(user)
+  if (list && list.length >= 1) {
+    return (
+      <table>
+      <thead>
+        <tr>
+          <th style={{ width: '30%' }}>Name</th>
+          <th style={{ width: '30%' }}>Email</th>
+          <th style={{ width: '10%' }}>Role</th>
+          <th style={{ width: '30%' }}>Ref</th>
+        </tr>
+      </thead>
+      <tbody>
+      {list.map((item) => {
+        return (
+        <tr>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+          <td>{item.role}</td>
+          <td>{item.guid}</td>
+        </tr>
+        )
+      })}
+      </tbody>
+      </table>
+    )
+  }
 
 
-	return <table><tr><td>Name</td><td>Email</td><td>Role</td></tr></table>
+  return <table><tr><td>Name</td><td>Email</td><td>Role</td></tr></table>
 }
 
