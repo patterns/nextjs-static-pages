088d2b0aaa1a18f15309a2a5ca021f7d479fd457
興怡 <>
2024-06-24 00:28:16 -0700

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,6 @@ export default function Home() {
 
 
   return (
-    <form action={loadUsers}>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -90,6 +89,7 @@ export default function Home() {
           </p>
         </a>
 
+        <form action={loadUsers}>
         <button type="submit"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
@@ -102,7 +102,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
             Display example users from Cloudflare D1 table.
           </p>
-        </button>
+        </button></form>
       </div>
     </main>
   );
