60fb593db215abfc601b1392c17d612c9c23886a
興怡 <>
2024-06-23 21:27:51 -0700

    revert identify back to server comp

--- src/app/ui/identify-check.tsx
+++ src/app/ui/identify-check.tsx
@@ -1,13 +1,11 @@
 import { headers } from 'next/headers'
 import { fetchIdentify } from '@/app/lib/data'
-import { atom, useSetAtom } from 'jotai'
 
-const tokenAtom = atom('')
 
 
 export default async function Identify() {
 	const headersList = headers()
-	const setToken = useSetAtom(tokenAtom)
+
 	if (headersList.has('cf-access-jwt-assertion')) {
 		const token = headersList.get('cf-access-jwt-assertion')
 		let authorization = ""
@@ -17,7 +15,7 @@ export default async function Identify() {
 		const session = await fetchIdentify(authorization)
 
 		// Store the token
-		setToken(authorization)
+
 
 		const debug = JSON.stringify({ member: session.data, token: session.token })
 		if (debug) {
