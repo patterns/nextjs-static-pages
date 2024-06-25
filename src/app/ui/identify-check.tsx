bd39cde35c66a4c313ac057aed18404fd8068d2a
興怡 <>
2024-06-24 23:48:35 -0700

    drop token from debug

--- src/app/ui/identify-check.tsx
+++ src/app/ui/identify-check.tsx
@@ -3,26 +3,23 @@ import { fetchIdentify } from '@/app/lib/data'
 
 
 export default async function Identify() {
-	const headersList = headers()
+    const headersList = headers()
 
-	if (headersList.has('cf-access-jwt-assertion')) {
-		const token = headersList.get('cf-access-jwt-assertion')
-		let authorization = ""
-		if (token) authorization = token
+    if (headersList.has('cf-access-jwt-assertion')) {
+        const token = headersList.get('cf-access-jwt-assertion')
+        let authorization = ""
+        if (token) authorization = token
 
-		// Forward the CF Access JWT to API to prove our identity
-		const session = await fetchIdentify(authorization)
+        // Forward the CF Access JWT to API to prove our identity
+        const session = await fetchIdentify(authorization)
 
-		// Store the token
+        const debug = JSON.stringify({ member: session.data })
+        if (debug) {
+            return <code className="font-mono font-bold">{debug}</code>
+        }
+    }
 
-
-		const debug = JSON.stringify({ member: session.data, token: session.token })
-		if (debug) {
-			return <code className="font-mono font-bold">{debug}</code>
-		}
-	}
-
-	return <code className="font-mono font-bold">Missing CF Access JWT header</code>
+    return <code className="font-mono font-bold">Missing CF Access JWT header</code>
 }
 
 
