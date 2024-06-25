e6b5ffda99dc33820aa411e7d986af8c8e132720
興怡 <>
2024-06-25 01:39:46 -0700

    action name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     if (token) {
       // copy header to cookie (probably not necessary, duplicated work)
       cookies().set('copiedjwt', token)
-
+      // access field from the submit button
       const action = formData.get('examples-action')
       if (action == "users") {
           redirect('/users')
@@ -42,7 +42,7 @@ export default function Home() {
         <TableSkeleton />
       </div>
 
-      <form action={loadUsers}>
+      <form action={loadExamples}>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
