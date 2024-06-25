4e3bbc2152275a67327ab0fe5c2cc96bfdb982a0
興怡 <>
2024-06-24 23:22:29 -0700

    button

--- src/app/(examples)/users/page.tsx
+++ src/app/(examples)/users/page.tsx
@@ -61,17 +61,15 @@ export default function Home() {
         <form action="">
         <button type="submit"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
+            Courses{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+            Display example courses from Cloudflare D1 table.
           </p>
         </button>
         </form>
