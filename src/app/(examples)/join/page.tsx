27ea98ca597fd82e81034e0a4668aececf7b90d3
興怡 <>
2024-06-26 02:06:11 -0700

    join button

--- src/app/(examples)/join/page.tsx
+++ src/app/(examples)/join/page.tsx
@@ -1,12 +1,12 @@
-
 import { headers, cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { Suspense } from 'react'
-import Identify from '@/app/ui/identify-check'
+////import { Suspense } from 'react'
+import Link from 'next/link'
+////import Users from '@/app/ui/users-list'
 import TableSkeleton from '@/app/ui/skeletons'
 export const runtime = 'edge'
 
-export default function Home() {
+export default function Page() {
   async function loadExamples(formData : FormData) {
     'use server'
 
@@ -15,6 +15,8 @@ export default function Home() {
     if (token) {
       // copy header to cookie (probably not necessary, duplicated work)
       cookies().set('copiedjwt', token)
+
+
       // access field from the submit button
       const action = formData.get('examples-action')
       if (action == "users") {
@@ -24,28 +26,30 @@ export default function Home() {
           redirect('/courses')
       }
       if (action == "join") {
-          redirect('/join')
+          // TODO  POST to API
+          redirect('/')
       }
+
     }
   }
 
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <form action={loadExamples}>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by inspecting&nbsp;
-          <Suspense fallback={<p>Connecting.....</p>}>
-          <Identify />
-          </Suspense>
+
+ 
+
         </p>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <TableSkeleton />
+
+        <input type="text" name="examples-name" placeholder="Isaac Newton" /><button type="submit" name="examples-action" value="join" />
+
       </div>
 
-      <form action={loadExamples}>
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -64,6 +68,7 @@ export default function Home() {
           </p>
         </a>
 
+
         <button type="submit" name="examples-action" value="courses"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
@@ -77,7 +82,6 @@ export default function Home() {
             Display example courses from Cloudflare D1 table.
           </p>
         </button>
-
         <button type="submit" name="examples-action" value="users"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
@@ -87,13 +91,17 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Display example users from Cloudflare D1 table.
           </p>
         </button>
 
-        <button type="submit" name="examples-action" value="join"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+
+        <Link
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors pointer-events-none"
+          aria-disabled="true"
+          tabIndex={-1}
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Join{" "}
@@ -102,10 +110,11 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Join new user example.
+            Join example user.
           </p>
-        </button>
-      </div></form>
+        </Link>
+      </div>
+        </form>
     </main>
   );
 }
