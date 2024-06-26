170a89a775a7d6c4da40b1c660da1cd5e74df64e
興怡 <>
2024-06-26 02:19:48 -0700

    button css

--- src/app/(examples)/join/page.tsx
+++ src/app/(examples)/join/page.tsx
@@ -46,7 +46,17 @@ export default function Page() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
 
-        <input type="text" name="examples-name" placeholder="Isaac Newton" /><button type="submit" name="examples-action" value="join" />
+        <input type="text" name="examples-name" placeholder="Isaac Newton" />
+        <button type="submit" name="examples-action" value="join"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            OK{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+        </button>
 
       </div>
 
