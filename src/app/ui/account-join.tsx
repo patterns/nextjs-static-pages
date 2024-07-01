c232f84d66dce677943390200776425ea479428b
興怡 <>
2024-06-30 22:29:56 -0700

    need cookie when cache is reset

--- src/app/ui/account-join.tsx
+++ src/app/ui/account-join.tsx
@@ -2,8 +2,8 @@ import { cookies, headers } from 'next/headers'
 import { fetchAccount } from '@/app/lib/data'
 
 export default async function Account() {
-/*******
-    // the cookie is a copy of the token from the initial identify call
+
+    // the cookie is a copy of the token
     const cookie = await cookies().get('copiedjwt')
     if (!cookie) {
         return <code>Missing cookie</code>
@@ -15,17 +15,6 @@ export default async function Account() {
     //     and insert into the members table with the role as PENDING.
 
     const list = await fetchAccount(cookie.value)
-****************************/
-    const headersList = headers()
-
-    if (!headersList.has('cf-access-jwt-assertion')) {
-        return <code>Missing header</code>
-    }
-    const token = headersList.get('cf-access-jwt-assertion')
-    let authorization = ""
-    if (token) authorization = token
-
-    const list = await fetchAccount(authorization)
     if (list) {
         const debug = JSON.stringify(list)
         return <code>{ debug }</code>
